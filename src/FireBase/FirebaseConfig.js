d9e182ce588d9436c2f9387131a2b3050d1767d8
SAADKAMRAN_05 <saadkamran874@gmail.com>
2025-08-22 21:12:16 +0500

    Update FirebaseConfig.js

--- src/FireBase/FirebaseConfig.js
+++ src/FireBase/FirebaseConfig.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage"; // If you want Storage
 
 // Your Firebase config (from Firebase Console)
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -23,4 +23,5 @@ const app = initializeApp(firebaseConfig);
 
 
  export  {db, auth, collection, addDoc, getDocs, query, orderBy, where, doc, updateDoc,onAuthStateChanged, signInWithPopup, GoogleAuthProvider} ;
+
 
